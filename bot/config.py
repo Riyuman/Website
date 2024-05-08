7ae03e25ebc34c98bb2915a1aed2d127b3d4d4c9
Riyuman <139713314+Riyuman@users.noreply.github.com>
2024-05-08 08:14:48 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 class Telegram:
-    API_ID = int(getenv("API_ID"))
-    API_HASH = getenv("API_HASH")
-    BOT_TOKEN = getenv("BOT_TOKEN")
-    SESSION_STRING = getenv("SESSION_STRING")
+    API_ID = int(getenv("25898132"))
+    API_HASH = getenv("490259e3399eb541c98cf204ddf92d75")
+    BOT_TOKEN = getenv("6944321971:AAGrk5upAwXsGfMpnKaMTsv1dRVDtz4H7xI")
+    SESSION_STRING = getenv("BQGLLJQALmWOSqr8ZAtaQJjlphCa6MyVEUojdUgH46_ILAMcvOO8X8jn9A5tC8TqEhRUiBBlnusKoJBlrQBm_06S0tSBWVZvd1hABZ2zNm_rCBLzSsE1BWc9N7qSUSVErYmEbB_wxkSTJ8hMnUncKbr4B3C4SK6wLckRy0_xk7bBGg6wh83kc_orOO5j7ZeRFYaXXdUEvf9Isj8hoxOEmey8CVbZUf3pZXU0YyaBybXdHBh6hDlODlJBa8z-fogX19iuVUPeXfoeDyOwzMQDcTOw-okgEhp2ikSg_rvi1KQTAhQeAA_6juwo1tv80WJyXF-DmtBfszC0daTyhODN7cCzf5LA2QAAAAE9lkijAA")
     PORT = int(getenv("PORT", "8080"))
     BASE_URL = getenv("BASE_URL").rstrip('/')
     AUTH_CHANNEL = getenv("1968459131").split(", ")
